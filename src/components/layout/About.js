5d7a322b036ce145c04003590c3f70f72b033a34
Hably Robby Rodhiyya <hablyrobby@gmail.com>
2019-06-17 21:05:21 +0700

    Final

--- src/components/layout/About.js
+++ src/components/layout/About.js
@@ -7,7 +7,11 @@ import React, {
     export const About = () => {
     return (
       <div className="mx-3">
-        <h5 className="font-weight-bold text-center m-3">About Muslim PRO</h5>
+          <div class="text-center">
+            <br/>
+            <img src="https://is5-ssl.mzstatic.com/image/thumb/Purple113/v4/40/6c/95/406c95f2-6428-860e-c2e1-133896c46d2d/AppIcon-0-1x_U007emarketing-0-0-85-220-0-7.png/246x0w.jpg" class="rounded" alt="img-thumbnail"  />
+          </div>
+        <h5 className="font-weight-bold text-center m-3">Tentang Muslim PRO</h5>
         <div className="m-3">
           <p>Diakui oleh lebih dari 70 juta Muslim di seluruh dunia sebagai aplikasi pengingat waktu sholat dan adzan terakurat, 
             Muslim Pro juga memiliki fitur Al-Qur'an bertulisan Arab lengkap dengan fonetik, terjemahan, serta resitasi audio. Selain itu, 
@@ -16,4 +20,3 @@ import React, {
       </div>
     );
   }
-
